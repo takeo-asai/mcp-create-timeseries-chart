f46e9a701aa44e73df2f8296fb9b7f85d8143644
Takeo Asai <spitson+github@gmail.com>
2025-05-03 22:49:03 +0900

    feat: 上部のグラフ作成を外部ファイル化

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 from src.stock import load_dataframe
 from src.ssm import caluculate_trend_level
-from src.alt import create_trend_season_chart
+from src.alt import create_trend_season_chart, create_actual_level_chart
 
 
 def main():
@@ -11,22 +11,11 @@ def main():
     forecast_df = caluculate_trend_level(df)
     forecast_df = forecast_df[forecast_df['date'] >= "2024-06-01"]
 
-    # グラフ汎用設定
-    x_axis = alt.X('date:T',  axis=alt.Axis(format='%Y-%m', title='日付', tickCount=6))
-    y_axis = alt.Y('v:Q', scale=alt.Scale(zero=False), axis=alt.Axis(title='値'))
-    v_legend = alt.Legend(title='レベル・実測値', orient='right', symbolStrokeWidth=5, labelFontSize=14, titleFontSize=14)
-    v2_legend = alt.Legend(title='トレンド・季節', orient='right', symbolStrokeWidth=5, labelFontSize=14, titleFontSize=14)
-
-    # 上部のグラフ
-    actual_level_df = pd.melt(forecast_df, id_vars=['date'], value_vars=['price', 'level'], var_name='c', value_name='v')
-    actual_level_chart = alt.Chart(actual_level_df).mark_line().encode(x=x_axis, y=y_axis, color=alt.Color('c:N', legend=v_legend, scale=alt.Scale(scheme='tableau10')))
-    confidence_band =  alt.Chart(forecast_df).mark_area(opacity=0.5, color='lightblue').encode(x=x_axis, y='lower_ci:Q', y2='upper_ci:Q')
-
     # グラフを縦に結合
-    upper_chart = (actual_level_chart + confidence_band).properties(title='サンプル時系列グラフ', width=800, height=300)
+    upper_chart = create_actual_level_chart(forecast_df)
     lower_chart = create_trend_season_chart(forecast_df)
-
     final_chart = alt.vconcat(upper_chart, lower_chart).resolve_scale(y='independent', color='independent')
+
     final_chart.save("output.svg")
     final_chart.save("output.png")
 
--- src/alt.py
+++ src/alt.py
@@ -1,6 +1,22 @@
 import altair as alt
 import pandas as pd
 
+# TODO: 決め打ちで書いている部分を変数化する
+
+def create_actual_level_chart(df: pd.DataFrame) -> alt.Chart:
+    # グラフ汎用設定
+    x_axis = alt.X('date:T',  axis=alt.Axis(format='%Y-%m', title='日付', tickCount=6))
+    y_axis = alt.Y('v:Q', scale=alt.Scale(zero=False), axis=alt.Axis(title='値'))
+    v_legend = alt.Legend(title='レベル・実測値', orient='right', symbolStrokeWidth=5, labelFontSize=14, titleFontSize=14)
+
+    # 上部のグラフ
+    actual_level_df = pd.melt(df, id_vars=['date'], value_vars=['price', 'level'], var_name='c', value_name='v')
+    actual_level_chart = alt.Chart(actual_level_df).mark_line().encode(x=x_axis, y=y_axis, color=alt.Color('c:N', legend=v_legend, scale=alt.Scale(scheme='tableau10')))
+    confidence_band =  alt.Chart(df).mark_area(opacity=0.5, color='lightblue').encode(x=x_axis, y='lower_ci:Q', y2='upper_ci:Q')
+    upper_chart = (actual_level_chart + confidence_band).properties(title='サンプル時系列グラフ', width=800, height=300)    
+    return upper_chart
+
+
 def create_trend_season_chart(df: pd.DataFrame) -> alt.Chart:
     # 下部のグラフ
     x_axis = alt.X('date:T',  axis=alt.Axis(format='%Y-%m', title='日付', tickCount=6))

efafdd48c5b1251cee9f97147e6cc4e3fd5e937d
Takeo Asai <spitson+github@gmail.com>
2025-05-04 05:18:33 +0900

    feat: 出力も引数で渡せるよう変更

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from src.ssm import caluculate_trend_level
 from src.alt import create_trend_season_chart, create_actual_level_chart, concat_chart_vertically
 
 
-def main(ticker, period):
+def main(ticker, period, output):
     # データの取得
     df = load_dataframe(args.ticker, period=args.period)
     forecast_df = caluculate_trend_level(df)
@@ -16,14 +16,14 @@ def main(ticker, period):
     lower_chart = create_trend_season_chart(forecast_df)
     final_chart = concat_chart_vertically(upper_chart, lower_chart)
 
-    final_chart.save("output.svg")
-    final_chart.save("output.png")
+    final_chart.save(output)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate a time series chart for a stock ticker.")
     parser.add_argument("ticker", type=str, nargs="?", default="AAPL", help="Stock ticker symbol (e.g., AAPL)")
     parser.add_argument("period", type=str, nargs="?", default="2y", help="Time period (e.g., 2y, 1y, 6mo)")
+    parser.add_argument("--output", type=str, default="output.svg", help="Output file name without extension (default: 'output')")
     args = parser.parse_args()
 
-    main(args.ticker, args.period)
+    main(args.ticker, args.period, args.output)

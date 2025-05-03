bd9eff2919849760449aa8a6da13c928740b4a09
Takeo Asai <spitson+github@gmail.com>
2025-05-03 20:54:48 +0900

    feat: カラーパレット変更

--- main.py
+++ main.py
@@ -65,20 +65,20 @@ def main():
     # グラフ汎用設定
     x_axis = alt.X('x_values:T',  axis=alt.Axis(format='%Y-%m', title='日付', tickCount=6))
     y_axis = alt.Y('v:Q', scale=alt.Scale(zero=False), axis=alt.Axis(title='値'))
-    v_legend = alt.Legend(title='カテゴリ', orient='right', symbolStrokeWidth=5, labelFontSize=14, titleFontSize=14)
-    v2_legend = alt.Legend(title='カテゴリ', orient='right', symbolStrokeWidth=5, labelFontSize=14, titleFontSize=14)
+    v_legend = alt.Legend(title='レベル・実測値', orient='right', symbolStrokeWidth=5, labelFontSize=14, titleFontSize=14)
+    v2_legend = alt.Legend(title='トレンド・季節', orient='right', symbolStrokeWidth=5, labelFontSize=14, titleFontSize=14)
 
     # 上部のグラフ
     actual_level_df = pd.melt(forecast_df, id_vars=['x_values'], value_vars=['y_values', 'level'], var_name='c', value_name='v')
-    actual_level_chart = alt.Chart(actual_level_df).mark_line().encode(x=x_axis, y=y_axis, color=alt.Color('c:N', legend=v_legend))
-    confidence_band =  alt.Chart(forecast_df).mark_area(opacity=0.4, color='lightblue').encode(x=x_axis, y='lower_ci:Q', y2='upper_ci:Q')
+    actual_level_chart = alt.Chart(actual_level_df).mark_line().encode(x=x_axis, y=y_axis, color=alt.Color('c:N', legend=v_legend, scale=alt.Scale(scheme='tableau10')))
+    confidence_band =  alt.Chart(forecast_df).mark_area(opacity=0.5, color='lightblue').encode(x=x_axis, y='lower_ci:Q', y2='upper_ci:Q')
 
     # 下部のグラフ
     trend_season_df = pd.melt(forecast_df, id_vars=['x_values'], value_vars=['trend', 'seasonal_7', 'seasonal_30', 'seasonal_90', 'seasonal_365'], var_name='c', value_name='v')
-    trend_season_chart = alt.Chart(trend_season_df).mark_line().encode(x=x_axis, y=y_axis, color=alt.Color('c:N', legend=v2_legend))
+    trend_season_chart = alt.Chart(trend_season_df).mark_line().encode(x=x_axis, y=y_axis, color=alt.Color('c:N', legend=v2_legend, scale=alt.Scale(scheme='tableau10')))
 
     # グラフを縦に結合
-    upper_chart = (actual_level_chart + confidence_band).properties(title='AAPLの株価時系列グラフ', width=800, height=300)
+    upper_chart = (actual_level_chart + confidence_band).properties(title='サンプル時系列グラフ', width=800, height=300)
     lower_chart = trend_season_chart.properties(title='トレンド・季節成分', width=800, height=300)
 
     final_chart = alt.vconcat(upper_chart, lower_chart).resolve_scale(y='independent', color='independent')
